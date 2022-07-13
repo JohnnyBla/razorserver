70d4dcaba16be0049bbee4197e53e2e4f02c5599
Johnny Blakley <majhack1990@gmail.com>
2022-07-13 13:04:25 -0400

    error

--- app.js
+++ app.js
@@ -33,18 +33,18 @@ connect.then(
 
 // view engine setup
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization,'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, PUT'
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization,'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PATCH, DELETE, PUT'
+//   );
+//   next();
+// });
 
 // Secure traffic only
 // app.all('*', (req, res, next) => {
