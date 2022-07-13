d079b5e5aca03e6e45e6cda9b6add4fb491c0651
Johnny Blakley <majhack1990@gmail.com>
2022-07-13 08:01:24 -0400

    error update

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
