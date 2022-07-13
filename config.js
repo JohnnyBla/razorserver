8baecf9b3065423dd2d04bec8fa6f1eae54bee76
Johnny Blakley <majhack1990@gmail.com>
2022-07-12 22:47:37 -0400

    views

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  secretKey: '_Super_Secret_Key_Name',
+  mongoUrl:
+    'mongodb+srv://majhack1990:Kameron0301.@cluster0.bbns9iq.mongodb.net/loads?retryWrites=true&w=majority',
+  'google': {
+    authRequired: false,
+    auth0Logout: true,
+    secret: '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',
+    baseURL: 'http://3.86.25.200/api/users/google/token/redirect',
+    clientID:
+      '711176198082-ukschgsf535j2v6m22snbvvgqos8b1qi.apps.googleusercontent.com',
+    issuerBaseURL: 'https://dev-nk95boyo.us.auth0.com',
+  },
+  mailer: {
+    service: 'hotmail',
+    pass: 'Rushelle42.',
+    user: 'razordispatch@gmail.com',
+  },
+};
