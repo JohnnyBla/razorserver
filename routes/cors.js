3504975881da1eeeb95f627b1160be70959ed067
Johnny Blakley <majhack1990@gmail.com>
2022-07-14 18:06:00 -0400

    OptionMethods

--- routes/cors.js
+++ routes/cors.js
@@ -5,12 +5,10 @@ const whitelist = [
   'https://localhost:3443',
   'http://localhost:3005',
 ];
-const corsOptionsDelegate = (req, callback, next) => {
+const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
   console.log(req.header('Origin'));
-  if (req.method === 'OPTIONS') {
-    return next();
-  }
+  
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true };
   } else {
