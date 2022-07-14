647f7f1c3237355aa0b1807f0fbcd49c5dc139ff
Johnny Blakley <majhack1990@gmail.com>
2022-07-14 19:25:20 -0400

    smtpMailService

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -16,8 +16,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.user,
-    pass: process.env.pass,
+    user: 'razordispatch@outlook.com',
+    pass: 'Rushelle42.',
   },
 });
 
